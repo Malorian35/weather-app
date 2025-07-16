41e8875833e9ee686c5d38db8c79a6d8a0e432ec
Malorian35 <malorian.3516.arms@gmail.com>
2025-07-16 19:09:04 +0300

    Устранена проблема с поиском города

--- src/api/geocodingApi.ts
+++ src/api/geocodingApi.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { City } from '../types/weatherTypes';
 
 const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
-const BASE_URL = 'http://api.openweathermap.org/geo/1.0';
+const BASE_URL = 'https://api.openweathermap.org/geo/1.0';
 
 export const searchCities = async (query: string): Promise<City[]> => {
   if (!API_KEY) throw new Error('API key is not configured');
